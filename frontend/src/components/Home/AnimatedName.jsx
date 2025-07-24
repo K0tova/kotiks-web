4ab1ec7cdaf63cb072d4e80b6531d8570cf39796
Kotova, Olga <Kotova.Olga+bcgprod@bcg.com>
2025-07-24 16:21:34 +0100

    feat(vercel): optimise for phone

--- frontend/src/components/Home/AnimatedName.jsx
+++ frontend/src/components/Home/AnimatedName.jsx
@@ -2,30 +2,35 @@ import React from 'react';
 import styles from './AnimatedName.module.css';
 
 const AnimatedName = ({ text }) => {
+     let delay = 0;
      return (
           <h2 className={styles.title} aria-label={text}>
-               {text.split('').map((char, idx) => {
-                    const isSpace = char === ' ';
-                    if (isSpace) {
-                         return (
-                              <React.Fragment key={idx}>
+               {text.split(' ').map((word, wIdx) => {
+                    const wordLetters = word.split('');
+                    return (
+                         <span key={wIdx} className={styles.word}>
+                              {wordLetters.map((char, idx) => {
+                                   const d = delay;
+                                   delay += 0.06;
+                                   return (
+                                        <span
+                                             key={idx}
+                                             className={styles.letter}
+                                             style={{ animationDelay: `${d}s` }}
+                                        >
+                                             {char}
+                                        </span>
+                                   );
+                              })}
+                              {/* space between words */}
+                              {wIdx !== text.split(' ').length - 1 && (
                                    <span
                                         className={`${styles.letter} ${styles.space}`}
-                                        style={{ animationDelay: `${idx * 0.06}s` }}
+                                        style={{ animationDelay: `${delay}s` }}
                                    >
                                         &nbsp;
                                    </span>
-                                   <wbr />
-                              </React.Fragment>
-                         );
-                    }
-                    return (
-                         <span
-                              key={idx}
-                              className={styles.letter}
-                              style={{ animationDelay: `${idx * 0.06}s` }}
-                         >
-                              {char}
+                              )}
                          </span>
                     );
                })}
